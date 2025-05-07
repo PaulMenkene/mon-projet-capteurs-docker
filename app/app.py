b13d35e98da3af400e5624a9b7fd23d9453bd3ee
PaulMenkene <155910941+PaulMenkene@users.noreply.github.com>
2025-05-07 12:41:06 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -59,5 +59,6 @@ def healthcheck():
         app.logger.error(f"Erreur dans /healthcheck : {e}")
         return {'status': 'error'}, 500
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
